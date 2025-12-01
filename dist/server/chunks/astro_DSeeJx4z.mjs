7dc22ff5fdb34adf214aac758c933149321f8bf8
khyberwear <atifjan2019@gmail.com>
2025-12-01 15:31:59 +0500

    Remove unused pages and components to streamline the project structure, including product detail, services, settings, thank you, and robots.txt pages. Also, delete the renderers module and CardInsight component to eliminate redundancy and improve maintainability.

--- dist/server/chunks/astro_DSeeJx4z.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_CHNLwNGm.mjs';
-import 'clsx';
--- dist/server/pages/_---slug_.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { p as page } from '../chunks/index_Ddnq9vtd.mjs';
-export { renderers } from '../renderers.mjs';
